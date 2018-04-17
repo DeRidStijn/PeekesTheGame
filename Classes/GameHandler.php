cf5bc42a62609bbabac5b31e2d3aa980667d8ce1
DeRidStijn <deridder.stijn93@gmail.com>
2018-04-17 16:36:54 +0200

    added basic functionality to check if char can win the game

--- Classes/GameHandler.php
+++ Classes/GameHandler.php
@@ -2,8 +2,43 @@
 
 class GameHandler
 {
-    public function __construct()
+    protected $characters = [];
+    protected $characterTurn, $gameTurn;
+    public function __construct(string $char1Name, string $char2Name)
     {
-        
+        $this->characters ['character1'] = new Character($char1Name);
+        $this->characters ['character2'] = new Character($char2Name);
+        $this->characterTurn = 1;
+        $this->gameTurn = 0;
+    }
+
+    public function otherCharactersTurn(){
+        if ($this->checkIfGameIsWon() === true){
+            echo 'tis gedaan';
+        }
+
+        else{
+            if ($this->characterTurn == 1){
+                $this->characterTurn = 2;
+            }
+            else{
+                $this->characterTurn = 1;
+            }
+        }
+    }
+
+    public function nextGameTurn(){
+        $this->gameTurn += 1;
+    }
+
+    public function checkIfGameIsWon(){
+        foreach ($this->characters as $char){
+            if($char->checkHealth >= 0) {
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
     }
 }
--- Tests/GameHandlerTest.php
+++ Tests/GameHandlerTest.php
@@ -1,30 +1,40 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: deridstijn
- * Date: 4/17/18
- * Time: 2:00 PM
- */
-
+/*spl_autoload_register(function($classname) {
+    require_once __DIR__ . '/PHPUnit/* ';
+    echo "Want to load $classname.\n";
+    throw new Exception("Unable to load $classname.");
+    $file = __DIR__ . "/Classes" . "{$classname}.php";
+    if (file_exists($file)) {
+        require_once($file);
+    }
+});*/
 
 class GameHandlerTest extends \PHPUnit\Framework\TestCase
 {
     protected $gameHandler;
-    public function initGameController(){
+    public function setup(){
         $this->gameHandler = new GameHandler('joske', 'jefke');
     }
-
+    /**
+     * @test
+     */
     public function otherCharactersTurn(){
         $this->gameHandler->nextCharacterTurn();
-        $nextPlayer = $this->gameHandler->getPlayersTurn();
+        $nextPlayer = $this->gameHandler->getCharacterTurn();
         $expectedResult = 'jefke';
         $this->assertEquals($expectedResult, $nextPlayer, "the next character does not seem to be jefke");
     }
 
+    /**
+     * @test
+     */
     public function nextGameTurn(){
         $this->gameHandler->nextGameTurn();
         $turn = $this->gameHandler->getGameTurn();
         $this->assertEquals(1, $turn, "the next turn does not seem to be turn 1");
     }
 
+    public function characterCanWinTheGame(){
+        
+    }
 }
