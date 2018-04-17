00ac03f98dbc6fe585ed1688f16457b1c869048d
DeRidStijn <deridder.stijn93@gmail.com>
2018-04-17 16:54:31 +0200

    made getters and setters for character so can take damage can be tested

--- Classes/Character.php
+++ Classes/Character.php
@@ -1,7 +1,28 @@
 <?php
 
-
 class Character
 {
+    protected $health;
+    protected $weapon;
+    protected $armor;
 
+    public function __construct(int $health, Weapon $weapon, Armor $armor)
+    {
+        $this->health = $health;
+        $this->weapon = new Weapon($weapon);
+        $this->armor = new Armor($armor);
+    }
+
+    public function getHealth(){
+        return $this->health;
+    }
+
+    public function takeDamage($damage){
+        $this->health -= $damage;
+    }
+
+    public function attack(){
+        return $this->weapon->getDamage();
+    }
+    
 }
--- Tests/GameHandlerTest.php
+++ Tests/GameHandlerTest.php
@@ -35,6 +35,6 @@ class GameHandlerTest extends \PHPUnit\Framework\TestCase
     }
 
     public function characterCanWinTheGame(){
-        
+
     }
 }
