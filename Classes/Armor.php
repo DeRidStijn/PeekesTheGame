89df246723bbfb37686c6ba0c1f4f6f611da8242
DeRidStijn <deridder.stijn93@gmail.com>
2018-04-17 13:59:49 +0200

    created empty classes to use later

---
+++ Classes/Armor.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: deridstijn
+ * Date: 4/17/18
+ * Time: 1:57 PM
+ */
+
+class Armor /*extends item*/
+{
+
+}
---
+++ Classes/Item.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: deridstijn
+ * Date: 4/17/18
+ * Time: 1:56 PM
+ */
+
+class Item
+{
+
+}
